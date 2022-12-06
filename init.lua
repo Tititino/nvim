2257eefed5e6218e1ab59489de22999bf1cbbb72
tino <mod01@outlook.it>
2022-12-06 17:24:27 +0100

    made some changes

--- init.lua
+++ init.lua
@@ -109,13 +109,13 @@ local ht = require('haskell-tools')
 local def_opts = { noremap = true, silent = true, }
 ht.setup {
   	hls = {
-    		-- See nvim-lspconfig's  suggested configuration for keymaps, etc.
+    		-- See nvim-lspconfig's suggested configuration for keymaps, etc.
     		on_attach = function(client, bufnr)
       			local opts = vim.tbl_extend('keep', def_opts, { buffer = bufnr, })
       			-- haskell-language-server relies heavily on codeLenses,
       			-- so auto-refresh (see advanced configuration) is enabled by default
-      			vim.keymap.set('n', '<space>ca', vim.lsp.codelens.run, opts)
-      			vim.keymap.set('n', '<space>hs', ht.hoogle.hoogle_signature, opts)
+      			vim.keymap.set('n', '<leader>ca', vim.lsp.codelens.run, opts)
+      			vim.keymap.set('n', '<leader>hs', ht.hoogle.hoogle_signature, opts)
       			-- default_on_attach(client, bufnr)  -- if defined, see nvim-lspconfig
     		end,
   	},
@@ -140,3 +140,9 @@ vim.api.nvim_create_autocmd('FileType', {
 		end
   	end 
 })
+
+-- augroup remember_folds
+--   autocmd!
+--   autocmd BufWinLeave ?* mkview
+--   autocmd BufWinEnter ?* silent! loadview
+-- augroup END
